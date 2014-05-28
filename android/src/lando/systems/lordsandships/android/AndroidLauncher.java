202b70cb71cbf51dfcddeec4b16aa09faae9a196
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-05-27 21:21:47 -0500

    Initial commit, bare project out of LibGDX Setup

---
+++ android/src/lando/systems/lordsandships/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package lando.systems.lordsandships.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import lando.systems.lordsandships.LordsAndShips;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LordsAndShips(), config);
+	}
+}
---
+++ desktop/src/lando/systems/lordsandships/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lando.systems.lordsandships.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lando.systems.lordsandships.LordsAndShips;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LordsAndShips(), config);
+	}
+}
