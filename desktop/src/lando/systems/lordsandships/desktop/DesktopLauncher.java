37ae332d6b6f1fc38b22c35b31edc7f8b681ab0d
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-05-27 21:55:45 -0500

    Add window size constants

--- desktop/src/lando/systems/lordsandships/desktop/DesktopLauncher.java
+++ desktop/src/lando/systems/lordsandships/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package lando.systems.lordsandships.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import lando.systems.lordsandships.LordsAndShips;
+import lando.systems.lordsandships.utils.Constants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width  = Constants.win_width;
+		config.height = Constants.win_height;
 		new LwjglApplication(new LordsAndShips(), config);
 	}
 }
--- html/src/lando/systems/lordsandships/client/HtmlLauncher.java
+++ html/src/lando/systems/lordsandships/client/HtmlLauncher.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import lando.systems.lordsandships.LordsAndShips;
+import lando.systems.lordsandships.utils.Constants;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Constants.win_width, Constants.win_height);
         }
 
         @Override
