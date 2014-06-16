8dde84d19a67782a9277d6560f803b961774e795
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-06-15 21:13:13 -0500

    Set texture atlas filters to mipmap nearest/nearest

--- desktop/src/lando/systems/lordsandships/desktop/DesktopLauncher.java
+++ desktop/src/lando/systems/lordsandships/desktop/DesktopLauncher.java
@@ -2,6 +2,7 @@ package lando.systems.lordsandships.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import lando.systems.lordsandships.LordsAndShips;
 import lando.systems.lordsandships.utils.Constants;
@@ -9,6 +10,8 @@ import lando.systems.lordsandships.utils.Constants;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		TexturePacker.Settings settings = new TexturePacker.Settings();
+		settings.filterMin = Texture.TextureFilter.MipMapNearestNearest;
+		settings.filterMag = Texture.TextureFilter.MipMapNearestNearest;
 		settings.maxWidth = 1024;
 		settings.maxHeight = 1024;
 		TexturePacker.process(settings, "./images", "./atlas", "game");
