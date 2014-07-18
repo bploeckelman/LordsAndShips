defc3cd07a9992cd3229b5442c3ddc5a4f3fbaf9
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-07-18 00:43:26 -0500

    Refactor - move screenshot utils to desktop project

--- core/src/lando/systems/lordsandships/utils/Utilities.java
+++ core/src/lando/systems/lordsandships/utils/Utilities.java
@@ -1,20 +1,28 @@
 package lando.systems.lordsandships.utils;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-import java.nio.ByteBuffer;
 
 /**
  * Brian Ploeckelman created on 6/17/2014.
  */
 public class Utilities {
 
+	/**
+	 * Split a texture into regions of the specified size split along a grid
+	 * starting at the specified row and column, splitting into the specified
+	 * number of tiles in both x and y directions.
+	 *
+	 * @param texture The texture to split into regions
+	 * @param width The width (in pixels) of a single region
+	 * @param height The height (in pixels) of a single region
+	 * @param col The tile column of the first tile to extract
+	 * @param row The tile row of the first tile to extract
+	 * @param xTiles The number of tiles to extract horizontally
+	 * @param yTiles The number of tiles to extract vertically
+	 * @return The texture regions that the texture was split into
+	 */
 	public static TextureRegion[][] splitAndGet(Texture texture, int width, int height, int col, int row, int xTiles, int yTiles) {
 		TextureRegion[][] allRegions = TextureRegion.split(texture, width, height);
 		TextureRegion[][] regions = new TextureRegion[yTiles][xTiles];
@@ -24,61 +32,6 @@ public class Utilities {
 			}
 		}
 		return regions;
-	}
-
-	/**
-	 * Get a Pixmap for the specified region of the screen
-	 *
-	 * https://github.com/libgdx/libgdx/wiki/Take-a-Screenshot
-	 *
-	 * @param x Lower left corner x coordinate
-	 * @param y Lower left corner y coordinate
-	 * @param w Width of Pixmap
-	 * @param h Height of Pixmap
-	 * @param yDown Whether to flip the Pixmap upside down or not
-	 * @return A Pixmap corresponding to the specified region of the screen
-	 */
-	public static Pixmap getScreenshot(int x, int y, int w, int h, boolean yDown){
-		final Pixmap pixmap = ScreenUtils.getFrameBufferPixmap(x, y, w, h);
-
-		if (yDown) {
-			// Flip the pixmap upside down
-			ByteBuffer pixels = pixmap.getPixels();
-			int numBytes = w * h * 4;
-			byte[] lines = new byte[numBytes];
-			int numBytesPerLine = w * 4;
-			for (int i = 0; i < h; i++) {
-				pixels.position((h - i - 1) * numBytesPerLine);
-				pixels.get(lines, i * numBytesPerLine, numBytesPerLine);
-			}
-			pixels.clear();
-			pixels.put(lines);
-		}
-
-		return pixmap;
-	}
-
-	/**
-	 * Save a screenshot of the current game window
-	 *
-	 * https://github.com/libgdx/libgdx/wiki/Take-a-Screenshot
-	 */
-	private static int screenshotCounter = 0;
-	public static void saveScreenshot(){
-		try{
-			FileHandle fh;
-			String id;
-			do {
-				id = String.format("%02d", ++screenshotCounter);
-				fh = new FileHandle("LordsAndShips-Screenshot-" + id + ".png");
-			} while (fh.exists());
-
-			Pixmap pixmap = getScreenshot(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
-			PixmapIO.writePNG(fh, pixmap);
-			pixmap.dispose();
-		}catch (Exception e){
-			System.err.println("Error saving screenshot: " + e.getMessage() + "\n" + e.getStackTrace());
-		}
 	}
 
 }
