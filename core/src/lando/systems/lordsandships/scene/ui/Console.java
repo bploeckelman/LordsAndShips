a2fd38637b812666600fc0d85d31a9d03e9e6711
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2014-12-20 02:25:29 -0600

    Tweak console layout, add tweens, tab key toggles

--- core/src/lando/systems/lordsandships/scene/ui/Console.java
+++ core/src/lando/systems/lordsandships/scene/ui/Console.java
@@ -1,5 +1,9 @@
 package lando.systems.lordsandships.scene.ui;
 
+import aurelienribon.tweenengine.Timeline;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.equations.*;
+import aurelienribon.tweenengine.primitives.MutableFloat;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
@@ -10,6 +14,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import lando.systems.lordsandships.GameInstance;
+import lando.systems.lordsandships.tweens.Vector2Accessor;
 
 
 /**
@@ -18,7 +24,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.Align;
 public class Console implements TextField.TextFieldListener {
 
     private static final int num_lines = 6;
-    private static final float margin_left = 20f;
+    private static final float margin = 10f;
     private static final String title = "Console";
 
     int numLines;
@@ -29,6 +35,7 @@ public class Console implements TextField.TextFieldListener {
     Vector2 pos;
     Vector2 size;
     Window window;
+    MutableFloat consoleAlpha;
 
     Stage stage;
     Skin skin;
@@ -47,6 +54,7 @@ public class Console implements TextField.TextFieldListener {
 
         inputField = new TextField("", skin);
         inputField.setTextFieldListener(this);
+        inputField.setColor(0.1f, 0.1f, 0.1f, 1.0f);
 
         for (int i = 0; i < numLines; ++i) {
             textLabels[i] = new Label(" ", skin);
@@ -57,39 +65,63 @@ public class Console implements TextField.TextFieldListener {
         float text_height = inputField.getHeight();
         float padding = 2f;
 
-        size = new Vector2(stage.getWidth(), (numLines + 1) * (text_height + padding));
-        pos = new Vector2(0, stage.getHeight() - size.y);
+        size = new Vector2(stage.getWidth(), (numLines) * (text_height + padding));
+        pos = new Vector2(0, stage.getHeight());
 
         window = new Window(title, skin);
         window.setZIndex(0);
         window.setTitleAlignment(Align.center);
+        window.setColor(Color.GRAY);
         window.setPosition(pos.x, pos.y);
         window.setSize(size.x, size.y);
-        window.setColor(Color.GRAY);
-        window.setVisible(visible);
+        window.setKeepWithinStage(false);
+        window.setMovable(false);
+        window.setResizable(false);
 
         for (int i = numLines - 2; i >= 0; --i) {
-            Label label = textLabels[i];
+            window.add(new Label("", skin)).width(margin);
+            window.add(textLabels[i]).left()
+                      .width(stage.getWidth() - 2 * margin)
+                      .padTop(padding).padBottom(padding);
+            window.add(new Label("", skin)).width(margin);
             window.row();
-            window.add(label).width(size.x - (padding * 2))
-                  .padTop(padding).padBottom(padding)
-                  .fill();
         }
 
+        window.add(new Label("", skin)).width(margin);
+        window.add(inputField).left()
+                  .width(stage.getWidth() - 2 * margin)
+                  .padTop(padding * 2).fill();
+        window.add(new Label("", skin)).width(margin);
         window.row();
-        window.add(inputField).width(size.x - (padding * 2))
-              .padTop(padding * 2).padBottom(padding / 2)
-              .fill();
 
         stage.addActor(window);
+
+        consoleAlpha = new MutableFloat(0f);
     }
 
 
     public void update(float dt) {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.GRAVE)) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.TAB)) {
             visible = !visible;
-            window.setVisible(visible);
+            float y = stage.getHeight() - (visible ? size.y : 0f);
+            float a = visible ? 1f : 0f;
+            float duration = visible ? 0.33f : 0.66f;
+            Timeline.createParallel()
+                    .push(
+                        Tween.to(pos, Vector2Accessor.Y, duration)
+                                .target(y)
+                                .ease(Bounce.OUT)
+                    )
+                    .push(
+                        Tween.to(consoleAlpha, 0, duration)
+                                .target(a)
+                                .ease(Bounce.OUT)
+                    )
+                    .start(GameInstance.tweens);
         }
+
+        window.setPosition(pos.x, pos.y);
+        window.setColor(1, 1, 1, consoleAlpha.floatValue());
     }
 
     @Override
