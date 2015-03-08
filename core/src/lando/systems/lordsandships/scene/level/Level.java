3a605987b68be1a1b184ac460f35e0cc84db4c17
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2015-03-08 06:19:45 -0500

    Work in progress on BSP room container

--- core/src/lando/systems/lordsandships/scene/level/Level.java
+++ core/src/lando/systems/lordsandships/scene/level/Level.java
@@ -1,27 +1,34 @@
 package lando.systems.lordsandships.scene.level;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import lando.systems.lordsandships.scene.tilemap.Tile;
 import lando.systems.lordsandships.utils.Assets;
+
+import java.util.LinkedList;
 
 /**
  * Brian Ploeckelman created on 3/7/2015.
  */
 public class Level {
 
+    RectBSP     bsp;
+    Rectangle   bounds;
     Array<Room> rooms;
 
-
-    // TODO : pass in a desired level size (num rooms and level size)
     public Level() {
-        // TODO : generate rooms this way...
-        //Rectangle levelRect = new Rectangle(0, 0, levelWidth, levelHeight);
-        //Array<Rectangle> bspRects = partitionRect(levelRect, numRooms);
+        // TODO : pass in a desired level size (num rooms and level size)
+        final int level_width  = 500;
+        final int level_height = 500;
+        final int num_rooms    = 10;
+        bounds = new Rectangle(0, 0, level_width, level_height);
+        bsp = new RectBSP(bounds);
         //Array<Room> rooms = generateRooms(bspRects);
         //connectNeighbors(rooms);
 
@@ -40,36 +47,38 @@ public class Level {
         generateNeighbors();
     }
 
-    private void generateNeighbors() {
-        int num_neighbors = Assets.rand.nextInt(rooms.size - 1) + 1;
-        for (int i = 0; i < num_neighbors; ++i) {
-            int j = Assets.rand.nextInt(rooms.size);
-            int k = Assets.rand.nextInt(rooms.size);
-            if (j == k) { j = 0; }
-
-            Room room1 = rooms.get(j);
-            Room room2 = rooms.get(k);
-
-            // TODO : pick a room edge, find two tiles closest to that edge
-            // TODO : set tiles to door tiles, add connectivity info
-            int x1 = 0;
-            int y1 = 0;
-            int x2 = 0;
-            int y2 = 0;
-
-            room2.addNeighbor(room1, x1, y1);
-            room1.addNeighbor(room2, x2, y2);
-        }
-    }
+    // -------------------------------------------------------------------------
+    // Game Loop Methods
+    // -------------------------------------------------------------------------
 
     public void update(float delta) {
 
     }
 
+    LinkedList<RectBSP.RectLeaf> leaves = new LinkedList<RectBSP.RectLeaf>();
     public void render(SpriteBatch batch, Camera camera) {
         for (Room room : rooms) {
             room.render(batch, camera);
         }
+
+        Assets.shapes.setProjectionMatrix(camera.combined);
+        Assets.shapes.begin(ShapeRenderer.ShapeType.Line);
+
+        Assets.shapes.setColor(Color.RED);
+        leaves.clear();
+        leaves.push(bsp.root);
+        RectBSP.RectLeaf leaf;
+        while (!leaves.isEmpty()) {
+            leaf = leaves.pop();
+            if (leaf.child1 != null) leaves.push(leaf.child1);
+            if (leaf.child2 != null) leaves.push(leaf.child2);
+            Assets.shapes.rect(leaf.rect.x, leaf.rect.y, leaf.rect.width, leaf.rect.height);
+        }
+
+        Assets.shapes.setColor(Color.MAGENTA);
+        Assets.shapes.rect(bounds.x-5, bounds.y-5, bounds.width+5, bounds.height+5);
+
+        Assets.shapes.end();
     }
 
     public void renderDebug(Camera camera) {
@@ -85,6 +94,8 @@ public class Level {
         Assets.shapes.end();
     }
 
+    // -------------------------------------------------------------------------
+    // Implementation Methods
     // -------------------------------------------------------------------------
 
     private Room generateRoom(int x, int y, int width, int height) {
@@ -127,5 +138,95 @@ public class Level {
 
         return room;
     }
+
+    private void generateNeighbors() {
+        int num_neighbors = Assets.rand.nextInt(rooms.size - 1) + 1;
+        for (int i = 0; i < num_neighbors; ++i) {
+            int j = Assets.rand.nextInt(rooms.size);
+            int k = Assets.rand.nextInt(rooms.size);
+            if (j == k) { j = 0; }
+
+            Room room1 = rooms.get(j);
+            Room room2 = rooms.get(k);
+
+            // TODO : pick a room edge, find two tiles closest to that edge
+            // TODO : set tiles to door tiles, add connectivity info
+            int x1 = 0;
+            int y1 = 0;
+            int x2 = 0;
+            int y2 = 0;
+
+            room2.addNeighbor(room1, x1, y1);
+            room1.addNeighbor(room2, x2, y2);
+        }
+    }
+
+    // -------------------------------------------------------------------------
+    // EXTRACT US
+    // -------------------------------------------------------------------------
+
+    class RectBSP {
+
+        class RectLeaf {
+            Rectangle rect;
+            RectLeaf  parent, child1, child2;
+
+            public RectLeaf(RectLeaf parent, Rectangle rect) {
+                this.parent = parent;
+                this.rect   = rect;
+                this.child1 = null;
+                this.child2 = null;
+            }
+        }
+
+        RectLeaf root;
+
+        public RectBSP(Rectangle rootRect) {
+            root = new RectLeaf(null, rootRect);
+            partition();
+        }
+
+        private void partition() {
+            LinkedList<RectLeaf> leaves = new LinkedList<RectLeaf>();
+            leaves.push(root);
+
+            int iters = 0;
+            while (iters != 25) {
+                RectLeaf leaf = leaves.pop();
+                // Pick a side of the current rect to split on
+                boolean splitx = Assets.rand.nextBoolean();
+
+                // Pick a position to split at
+                // TODO : make sure there's enough room in each split to fit a room
+                float splitPlane = MathUtils.clamp(Assets.rand.nextFloat(), 0.2f, 0.8f)
+                                 * (splitx ? leaf.rect.width : leaf.rect.height);
+
+                Gdx.app.log("SPLIT", "Splitting rect = " + leaf.rect.toString());
+                // Create the two child leaves
+                Rectangle bounds1, bounds2;
+                if (splitx) {
+                    bounds1 = new Rectangle(leaf.rect.x              , leaf.rect.y,                   splitPlane, leaf.rect.height);
+                    bounds2 = new Rectangle(leaf.rect.x + splitPlane , leaf.rect.y, leaf.rect.width - splitPlane, leaf.rect.height);
+                    Gdx.app.log("SPLIT", "\ton x: " + splitPlane + "  bounds1 = " + bounds1.toString());
+                    Gdx.app.log("SPLIT", "\ton x: " + splitPlane + "  bounds2 = " + bounds2.toString());
+                } else {
+                    bounds1 = new Rectangle(leaf.rect.x, leaf.rect.y             , leaf.rect.width,                    splitPlane);
+                    bounds2 = new Rectangle(leaf.rect.x, leaf.rect.y + splitPlane, leaf.rect.width, leaf.rect.height - splitPlane);
+                    Gdx.app.log("SPLIT", "\ton y: " + splitPlane + "  bounds1 = " + bounds1.toString());
+                    Gdx.app.log("SPLIT", "\ton y: " + splitPlane + "  bounds2 = " + bounds2.toString());
+                }
+                leaf.child1 = new RectLeaf(leaf, bounds1);
+                leaf.child2 = new RectLeaf(leaf, bounds2);
+
+                leaves.push(leaf.child1);
+                leaves.push(leaf.child2);
+
+                //...repeat
+                ++iters;
+            }
+        }
+
+    }
+
 
 }
